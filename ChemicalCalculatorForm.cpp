81d29302038f11d22a0a97ab5246ab28b2d52d9d
Hapooga <86728973+Hapooga@users.noreply.github.com>
2022-06-27 22:16:12 +0300

    Add files via upload

---
+++ ChemicalCalculatorForm.cpp
@@ -0,0 +1,188 @@
+#include "ChemicalCalculatorForm.h"
+
+#include"FormulaParser.h"
+#include "Calculate.h"
+
+#include <stdexcept>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ChemicalCalculator::ChemicalCalculatorForm form;
+	Application::Run(% form);
+}
+
+
+
+//=======================================================================//mass_calculation
+System::Void ChemicalCalculator::ChemicalCalculatorForm::calculat_mass_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+
+	String^ molecular_weight;
+	
+		try {
+			if (buffer_formula != chemical_formula_textBox->Text) {
+
+				buffer_formula = chemical_formula_textBox->Text;
+
+				molecular_weight = gcnew String(ConvertToMolecularWeight(chemical_formula_textBox->Text));
+
+				buffer_molecular_weight = molecular_weight;
+
+				result_mass_calculation_textBox->Text = Mass(molecular_weight, 
+					volume_mass_calculation_textBox->Text, 
+					molar_concentration_mass_calculation_textBox->Text);
+
+			}
+			else {
+
+				result_mass_calculation_textBox->Text = Mass(buffer_molecular_weight, 
+					volume_mass_calculation_textBox->Text, 
+					molar_concentration_mass_calculation_textBox->Text);
+
+			}
+		}
+		catch (std::exception& ex) {
+			MessageBox::Show(gcnew String(ex.what()), "Ошибка!");
+		}
+	
+}
+
+System::Void ChemicalCalculator::ChemicalCalculatorForm::cleare_mass_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	volume_mass_calculation_textBox->Text = L"";
+	molar_concentration_mass_calculation_textBox->Text = L"";
+	result_mass_calculation_textBox->Text = L"";
+}
+
+
+
+
+
+//=======================================================================//volume_calculation
+System::Void ChemicalCalculator::ChemicalCalculatorForm::calculat_volume_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+
+	String^ molecular_weight;
+
+	try {
+		if (buffer_formula != chemical_formula_textBox->Text) {
+
+			buffer_formula = chemical_formula_textBox->Text;
+
+			molecular_weight = gcnew String(ConvertToMolecularWeight(chemical_formula_textBox->Text));
+
+			buffer_molecular_weight = molecular_weight;
+
+			result_volume_calculation_textBox->Text = Volume(molecular_weight, 
+				molar_concentration_volume_calculation_textBox->Text, 
+				mass_gramm_volume_calculation_textBox->Text);
+
+		}
+		else {
+			result_volume_calculation_textBox->Text = Volume(buffer_molecular_weight, 
+				molar_concentration_volume_calculation_textBox->Text, 
+				mass_gramm_volume_calculation_textBox->Text);
+		}
+	}
+	catch (std::exception& ex) {
+		MessageBox::Show(gcnew String(ex.what()), "Ошибка!");
+	}
+	
+}
+System::Void ChemicalCalculator::ChemicalCalculatorForm::cleare_volume_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	molar_concentration_volume_calculation_textBox->Text = L"";
+	mass_gramm_volume_calculation_textBox->Text = L"";
+	result_volume_calculation_textBox->Text = L"";
+}
+
+
+
+//=======================================================================//molar_concentration
+System::Void ChemicalCalculator::ChemicalCalculatorForm::calculat_molar_concentration_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+
+	String^ molecular_weight;
+
+	try {
+		if (buffer_formula != chemical_formula_textBox->Text) {
+
+			buffer_formula = chemical_formula_textBox->Text;
+			
+			molecular_weight = gcnew String(ConvertToMolecularWeight(chemical_formula_textBox->Text));
+
+			buffer_molecular_weight = molecular_weight;
+
+			result_molar_concentration_textBox->Text = MolarConcentration(molecular_weight, 
+				volume_molar_concentration_calculation_textBox->Text, 
+				mass_gramm_molar_concentration_calculation_textBox->Text);
+
+		}
+		else {
+
+			result_molar_concentration_textBox->Text = MolarConcentration(buffer_molecular_weight,
+				volume_molar_concentration_calculation_textBox->Text, 
+				mass_gramm_molar_concentration_calculation_textBox->Text);
+
+		}
+	}
+	catch (std::exception& ex) {
+		MessageBox::Show(gcnew String(ex.what()), "Ошибка!");
+	}
+
+	
+}
+System::Void ChemicalCalculator::ChemicalCalculatorForm::cleare_molar_concentration_calculation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	volume_molar_concentration_calculation_textBox->Text = L"";
+	mass_gramm_molar_concentration_calculation_textBox->Text = L"";
+	result_molar_concentration_textBox->Text = L"";
+}
+
+
+
+
+//=======================================================================//legation
+System::Void ChemicalCalculator::ChemicalCalculatorForm::calculat_legation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	try {
+		result_output_ligation_textBox->Text = MassInsertion(
+			vector_mass_ligation_textBox->Text, vector_lenght_ligation_textBox->Text,
+			ratio_l_ligation_textBox->Text, ratio_r_ligation_textBox->Text,
+			insertion_lenght_ligation_textBox->Text
+		);
+	}
+	catch (std::exception& ex) {
+		MessageBox::Show(gcnew String(ex.what()),L"Ошибка!");
+	}
+}
+System::Void ChemicalCalculator::ChemicalCalculatorForm::cleare_ligation_button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	vector_mass_ligation_textBox->Text = L"";
+	vector_lenght_ligation_textBox->Text = L"";
+	ratio_l_ligation_textBox->Text = L"";
+	ratio_r_ligation_textBox->Text = L"";
+	insertion_lenght_ligation_textBox->Text = L"";
+	result_output_ligation_textBox->Text = L"";
+}
+
+
+
+
+
+//=======================================================================//StripMenuItem
+System::Void ChemicalCalculator::ChemicalCalculatorForm::выходToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	this->Close();
+}
+System::Void ChemicalCalculator::ChemicalCalculatorForm::оПрограммеToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	MessageBox::Show("Калькулятор химический", "Hapooga Pet Project");
+}
+
---
+++ Tests.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#include <vector>
+#include <string>
+#include <string_view>
+
+#include "Tester.h"
+#include "FormulaParser.h"
+
+
+
+void TestParse() {
+
+	std::vector<std::string> formuls{
+		"H2S5(O4)",
+		"H2S(O4)",
+		"Cu(NO3)2*6(H2O)",
+		"HCO3−",
+		"HC5H5N+",
+		"[Cu(H2O)4]SO4*0.5H2O",
+		"[Cu(H2O)4]SO4*H2O",
+		"Cu(NO3)2*6H2O",
+		"Co3(Fe(CN)6)2",
+		"(NH4)2[Pt(SCN)6]",
+		"(NH4)2Hg(SCN)4",
+		"C4H11NO3*HCl",
+		"C18H36N4O11*H2SO4*H2O"
+	};
+
+	std::vector<std::string> results;
+	PeriodicTable periodic_table;
+	std::unique_ptr<Expression<float>> formula_expression;
+
+	for (const auto& formula : formuls) {
+		try {
+			formula_expression = Parse(std::string_view(formula), periodic_table);
+			results.push_back(std::to_string(formula_expression->Evaluate()));
+		}
+		catch(std::exception& ex) {
+			results.push_back(ex.what());
+		}
+	}
+
+	std::vector<std::string> comparation{
+		"Неверно установлены скобки",
+		"Неверно установлены скобки",
+		"Неверно установлены скобки (H2O)",
+		"61.015934",
+		"66.102646",
+		"240.674957",
+	    "249.682404",
+		"295.643280",
+		"600.708984",
+		"579.661499",
+		"469.003510",
+		"157.596283",
+		"600.593567"
+	};
+
+	for (int i = 0; i < comparation.size(); ++i) {
+		ASSERT_EQUAL(comparation[i], results[i]);
+	}
+}
+
